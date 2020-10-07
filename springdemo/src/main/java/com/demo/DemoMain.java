bc0df3fa3bade084c662776e490eaa6cab76fa10
Monesh Venkul <mvvenkul@gmail.com>
2020-10-07 10:58:15 +0530

    Added SpringDemo

---
+++ springdemo/src/main/java/com/demo/DemoMain.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author mvven
+ *
+ */
+public class DemoMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/demo/spring-config.xml");
+		Employee employee = (Employee) context.getBean("demo"); // new -- IOC 
+		employee.setEmpNo(51886600);
+		employee.setEmpName("Monesh");
+		System.out.println("Employee Number is : " + employee.getEmpNo());
+		System.out.println("Employee Number is : " + employee.getEmpName());
+		
+		
+
+	}
+
+}
---
+++ springdemo/src/main/java/com/demo/Employee.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author mvven
+ *
+ */
+public class Employee {
+	private int empNo;
+	private String empName;
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+
+}
